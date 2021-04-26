c1ef7fdbc12777491014117a11a89c25174c2eb1
Lucidium123 <70474631+Lucidium123@users.noreply.github.com>
2021-04-25 18:50:23 -0700

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -8,6 +8,7 @@ module.exports = {
     let embed = new MessageEmbed()
     .setTitle("Ping")
     .setDescription(`${client.ws.ping} MS`)
+    .setFooter("Made by: Lucid_Dev#1632 and Colderry#4096")
     .setColor("RANDOM")
 
     message.channel.send(embed)
