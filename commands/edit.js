3a167dbf2a3080c8284d5dba71676a22c9e12334
Lucidium123 <70474631+Lucidium123@users.noreply.github.com>
2021-04-26 06:12:46 -0700

    Update edit.js

--- commands/edit.js
+++ commands/edit.js
@@ -7,29 +7,57 @@ module.exports = {
   name: "edit",
 
   async run(client, message, args) {
-    if (!message.member.hasPermission("MANAGE_SERVER")) return message.channel.send("**You do not have permission to use this command...**")
-        if (!message.guild.me.hasPermission("MANAGE_SERVER")) return message.channel.send(`**I don\'t have the permission \`MANAGE_SERVER\` for this command to work i require it!**`)
-        
-    const embed = new MessageEmbed()
-    .setTitle("**Wrong Usage!!**")
-    .setDescription("\`\`\`p!edit <message ID> <new prize> <winner count>\`\`\`")
-    .setColor(red)
+
+    if (!message.member.hasPermission("MANAGE_GUILD")) {
+      return message.channel.send("**You do not have permission to use this command...**");
+    };
+
+    if (!message.guild.me.hasPermission("MANAGE_GUILD")) {
+      return message.channel.send(`**I don\'t have the permission \`MANAGE_GUILD\` for this command to work i require it!**`);
+    }
+
+    const err = new MessageEmbed()
+      .setTitle("**Wrong Usage!!**")
+      .setDescription("\`\`\`p!edit <message ID> <winner count> <new prize>\`\`\`")
+      .setColor(red)
 
     let messageID = args[0];
-    if (!messageID) return message.channel.send(embed)
+    let winner = args[1]
+    let prize = args.slice(1).join(" ");
 
-        client.giveawaysManager.edit(messageID, {
-            newWinnerCount: args[2],
-            newPrize: args[1],
-            addTime: 5000
-        }).then(() => {
-            const numberOfSecondsMax = client.giveawaysManager.options.updateCountdownEvery / 5000;
-            message.channel.send('Success! Giveaway will updated in less than ' + numberOfSecondsMax + ' seconds.');
-        }).catch((err) => {
-            message.channel.send('No giveaway found for ' + messageID + ', please check and try again');
-        });
+    if (!messageID) {
+      return message.channel.send(
+        err
+      );
+    };
 
-        if (!args[1]) return message.channel.send(embed)
-        if (!args[2]) return message.channel.send(embed)
-  }
-}
+    if (!winner) {
+      return message.channel.send(
+        err
+      );
+    };
+    if (!prize) {
+      return message.channel.send(
+        err
+      );
+    };
+
+
+    client.giveawaysManager.edit(messageID, {
+      newWinnerCount: winner,
+      newPrize: prize,
+      addTime: 5000
+    }).then(() => {
+      const numberOfSecondsMax = client.giveawaysManager.options.updateCountdownEvery / 5000;
+      return message.channel.send('Success! Giveaway will updated in less than ' + numberOfSecondsMax + ' seconds.');
+    }).catch((err) => {
+      message.channel.send(
+        new MessageEmbed()
+          .setTitle("Error")
+          .setColor("RED")
+          .setDescription(":x: Not an valid giveaway id. Recheck and try again!")
+          .setFooter(`ID: ` + messageID)
+      );
+    });
+  },
+};
