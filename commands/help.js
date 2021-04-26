06d973bd1206b2aa15facb5a0482e919dad6ce59
Lucidium123 <70474631+Lucidium123@users.noreply.github.com>
2021-04-25 18:50:09 -0700

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -9,6 +9,7 @@ module.exports = {
     .setDescription("\`ping\`, \`start\`, \`end\`, \`delete\`, \`reroll\`, \`edit\`")
     .addField("Invite The Bot", "[Link](https://discord.com/api/oauth2/authorize?client_id=759452171300372550&permissions=0&scope=bot)")
     .setTimestamp()
+    .setFooter("Made by: Lucid_Dev#1632 and Colderry#4096")
     .setColor("BLACK")
 
     message.channel.send(embed)
